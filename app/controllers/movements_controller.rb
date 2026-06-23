b3f9df53a37f63806c5efbfbe9fd0c51bc8588b4
Washington Arambillete <milanghdarkoh@hotmail.com>
2026-06-23 15:50:57 +0200

    202606231550 WSL ubuntu problem solved

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,15 +1,23 @@
 class MovementsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_movement, only: [ :edit, :update, :destroy, :show ]   # <-- incluido :show
 
-  def index
-    @movements = current_user.movements
-                             .includes(:source_account, :destination_account)
-                             .order(date: :desc)
-                             .limit(50)
+  ddef index
+  @movements = current_user.movements
+                           .includes(:source_account, :destination_account)
+                           .order(date: :desc, created_at: :desc)
+end
+
+  def show
+    # @movement ya está cargado por set_movement
   end
 
   def new
     @movement = Movement.new
+    @accounts = current_user.accounts.order(:name)
+  end
+
+  def edit
     @accounts = current_user.accounts.order(:name)
   end
 
@@ -23,7 +31,25 @@ class MovementsController < ApplicationController
     end
   end
 
+  def update
+    if @movement.update(movement_params)
+      redirect_to movements_path, notice: "Movimiento actualizado correctamente."
+    else
+      @accounts = current_user.accounts.order(:name)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @movement.destroy
+    redirect_to movements_path, notice: "Movimiento eliminado correctamente."
+  end
+
   private
+
+  def set_movement
+    @movement = current_user.movements.find(params[:id])
+  end
 
   def movement_params
     params.require(:movement).permit(:date, :description, :amount, :movement_type,
